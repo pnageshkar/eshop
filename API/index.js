73fcba9a7adb4128cb6912bce1a902fd1d16ce72
Prasad Nageshkar <prasad.nageshkar@gmail.com>
2023-05-16 20:59:10 +0530

    implemented Routes,Controllers and Middleware

--- API/index.js
+++ API/index.js
@@ -1,6 +1,34 @@
 const express = require('express');
 const app = express();
-const
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require("cors");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
 
-const port = 5000;
 
+
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => { console.log('Database is connected') })
+.catch((err) => { console.log('Can not connect to the database' + err) });
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripeRoute);
+
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log('listening on port ' + process.env.PORT);
+});
